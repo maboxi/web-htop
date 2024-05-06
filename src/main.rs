4b0c00f4c1ddc734a2e09209af1aa7a9697e7420
Maximilian Pöllinger <maxi.poellinger@gmail.com>
2024-05-06 14:25:26 +0000

    removed frontend from rust project

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use axum::{extract::{Path, State}, http::{header::{self, CONTENT_TYPE}, HeaderValue, Method, Response, StatusCode}, response::{Html, IntoResponse}, routing::get, Json, Router};
+use axum::{extract::State, http::{header::CONTENT_TYPE, Method}, response::IntoResponse, routing::get, Json, Router};
 use sysinfo::System;
 use tower_http::cors::{Any, CorsLayer};
 use tokio::sync::Mutex;
@@ -12,10 +12,7 @@ async fn main() {
 
 
     let app = Router::new()
-        .route("/", get(index_html_get))
-        .route("/index.mjs", get(index_mjs_get))
-        .route("/api/cpus", get(cpus_get))
-        .route("/static/*path", get(static_get))   
+       .route("/api/cpus", get(cpus_get))
         .with_state(shared_state)
         .layer(CorsLayer::new()
             .allow_origin(Any)
@@ -34,49 +31,6 @@ async fn main() {
 
 struct AppState {
     system: Mutex<System>
-}
-
-#[axum::debug_handler]
-async fn static_get(Path(path): Path<String>) -> impl IntoResponse {
-    let path = &format!("web/static/{}", path.trim_start_matches('/'));
-    let mime_type = mime_guess::from_path(path).first_or_text_plain();
-
-    print!("[GET] STATIC: Fetching {path} -> ");
-
-    match tokio::fs::read_to_string(path).await {
-        Err(err) => {
-            println!("Error: {}", err);
-            Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(format!("Error: {err}"))
-                .unwrap()
-        },
-        Ok(markup) => {
-            println!("Success! MIME Type: {:?}", HeaderValue::from_str(mime_type.as_ref()).unwrap());
-            Response::builder()
-                .status(StatusCode::OK)
-                .header(header::CONTENT_TYPE, HeaderValue::from_str(mime_type.as_ref()).unwrap(),)
-                .body(markup)
-                .unwrap()
-        }
-    }
-}
-
-#[axum::debug_handler]
-async fn index_html_get() -> impl IntoResponse {
-    let markup = tokio::fs::read_to_string("web/index.html").await.unwrap();
-
-    Html(markup)
-}
-
-#[axum::debug_handler]
-async fn index_mjs_get() -> impl IntoResponse {
-    let markup = tokio::fs::read_to_string("web/index.mjs").await.unwrap();
-
-    Response::builder()
-        .header("content-type", "application/javascript;charset=utf-8")
-        .body(markup)
-        .unwrap()
 }
 
 #[axum::debug_handler]
